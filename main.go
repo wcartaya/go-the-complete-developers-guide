0553d8d8004c93476b706c5811eccf137a15dbe9
wcartaya <wcartaya@gmail.com>
2021-11-03 22:31:54 -0300

    slice and convert type

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	/*
+		cards := newDeck()
+
+		hand, remainingCards := deal(cards, 5)
+
+		hand.print()
+		remainingCards.print()
+	*/
 	cards := newDeck()
-
-	cards.print()
-
+	fmt.Println(cards.toString())
 }
