320939484262be5aa62dd3663fc1bbc545dbf6fe
wcartaya <wcartaya@gmail.com>
2021-11-03 22:40:50 -0300

    saveToFile

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	/*
 		cards := newDeck()
@@ -12,5 +10,6 @@ func main() {
 		remainingCards.print()
 	*/
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	//fmt.Println(cards.toString())
+	cards.saveToFile("temp.txt")
 }
