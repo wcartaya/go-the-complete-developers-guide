826420df576eb80a2d9615ae8608ff72b27e1b58
wcartaya <wcartaya@gmail.com>
2021-11-04 23:53:06 -0300

    more test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -12,4 +15,23 @@ func TestNewDeck(t *testing.T) {
 	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected first card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktest")
+
+	d := newDeck()
+	d.saveToFile("_decktest")
+
+	loadedDeck := newDeckFromFile("_decktest")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktest")
 }
